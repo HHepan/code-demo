e609c14e4abcf1caf3fbcf215d1ad4ed1f8b6446
HHepan <250409258@qq.com>
2023-04-30 16:27:30 +0800

    init

--- angular/menu-frame-demo/src/app/app-routing.module.ts
+++ angular/menu-frame-demo/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LayoutComponent} from './part/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+      },
+      {
+        path: 'component1Path',
+        loadChildren: () => import('./component1/component1.module').then(m => m.Component1Module),
+      },
+      {
+        path: 'component2Path',
+        loadChildren: () => import('./component2/component2.module').then(m => m.Component2Module),
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
